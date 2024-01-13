ad36ac83976fb2935bc30cadca288a0b23989a20
alrema <alrema91@gmail.com>
2024-01-13 17:14:40 +0100

    Exercise 3.5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const apiPath = "/api/persons";
 
@@ -62,6 +63,26 @@ app.delete(`${apiPath}/:id`, (request, response) => {
     return person.id != id;
   });
   response.status(204).end();
+});
+
+app.post(apiPath, (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Bad body content. name and number must be providen",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 100000) + 1,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 app.get("/info", (request, response) => {
