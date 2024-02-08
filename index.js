19aa02901ab513ce2f3505d5c06939072594b92c
alrema <alrema91@gmail.com>
2024-02-08 12:49:24 +0100

    Exercise 3.11

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const morganMid = morgan(
   ":method :url :status :res[content-length] - :response-time ms :body "
 );
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 app.use(morganMid);
 
